1719c22b89c50d08cf52f863972a596a5ff8cd7e
Arael Roca Hanson <44608423+arocahan@users.noreply.github.com>
2024-11-17 00:35:01 +0100

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "You clicked the button! Have a great day, Arael!";
+    messageElement.style.color = "#0078D4";
+    messageElement.style.marginTop = "20px";
+}
